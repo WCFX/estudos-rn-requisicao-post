06fc93f09a284d2834ac4b67b46fee4c2fcd40e4
wcfx <wcfx.dev@gmail.com>
2020-11-02 20:01:29 -0300

    Requisicao feita com sucesso

--- src/screen/Home/index.js
+++ src/screen/Home/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, SafeAreaView, TextInput } from 'react-native';
+import { View, Text, SafeAreaView, TouchableOpacity, TextInput } from 'react-native';
 
 import styles from './style';
 
@@ -9,6 +9,30 @@ const Home = () => {
   const [title, setTitle ] = useState('');
   const [body, setBody ] = useState('');
 
+  
+  async function handleSend(){
+    if(title != '' && body != '') {
+
+      const req = await fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+          title: title,
+          body: body,
+          userId: 345
+        }),
+        headers:{
+          'Content-Type': 'application/json'
+        }
+      });
+
+      const json = await req.json();
+
+      alert(`Adicionado! ID ${json.id} - ${json.title}`);
+    } else {
+      alert("Preencha as informações.");
+    }
+  }
+
 
   return(
     <SafeAreaView style={styles.container}>
@@ -16,18 +40,20 @@ const Home = () => {
 
         <Text style={styles.titleText}>Título:</Text>
         <TextInput
-          style={styles.inputTitle}
+          style={styles.input}
           value={title}
           onChangeText={setTitle}
         />
-        
+
         <Text style={styles.titleText}>Corpo:</Text>
         <TextInput
-          style={styles.inputBody}
+          style={styles.input}
           value={body}
           onChangeText={setBody}
         />
-
+        <TouchableOpacity style={styles.button} onPress={handleSend}>
+          <Text style={styles.buttonText}>Enviar</Text>
+        </TouchableOpacity>
       </View>
 
     </SafeAreaView>
--- src/screen/Home/style.js
+++ src/screen/Home/style.js
@@ -4,7 +4,7 @@ import { StyleSheet } from 'react-native';
 const style = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#111',
+    backgroundColor: '#333',
   },
   containerInputs: {
     marginTop: 30,
@@ -15,20 +15,32 @@ const style = StyleSheet.create({
     fontSize: 22,
     marginBottom: 8,
   },
-  inputTitle:{
+  input:{
     marginBottom: 20,
     width: '90%',
-    height: 40,
+    height: 50,
     borderWidth: 1,
-    borderColor: '#ccc',
+    borderColor: '#555',
+    color: '#FFF',
+    fontSize: 18,
+    paddingHorizontal: 10,
     borderRadius: 8,
+    backgroundColor: '#555',
   },
-  inputBody:{
+  button: {
     width: '90%',
-    height: 40,
-    borderWidth: 1,
-    borderColor: '#fff',
+    height: 50,
+    backgroundColor: '#f00555',
     borderRadius: 8,
+    elevation: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: '#f9f9f9',
+    textAlign: 'center',
+    fontSize: 22,
+    letterSpacing: 1,
   },
   
 });
